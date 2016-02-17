5e1761955def775c4b9b26e0d8a4cbcb786678f9
Jakob Hanehøj <Jakob.hanehoj@gmail.com>
2016-02-17 15:18:53 +0100

    Changed PRGN for tables to use mersenne twister

--- Code/TabulationHash.cpp
+++ Code/TabulationHash.cpp
@@ -2,44 +2,48 @@
 
 #include <iostream>
 #include <time.h>
+#include <algorithm>
 
 using namespace std;
 
 TabulationHash::TabulationHash(unsigned maxStrLen)
 {
-	srand(time(NULL));
+
+  mt19937 generator(time(NULL));
 
 	// Initialize the tables to fit the given maximum string length.
 	tabulationTables = new HashEntry*[maxStrLen];
 	for (unsigned i = 0; i < maxStrLen; i++)
 		tabulationTables[i] = new HashEntry[entries];
 
-	unsigned r = (sizeof(tabulationTables)/sizeof(tabulationTables[0]));
-	unsigned c = (sizeof(tabulationTables[0])/sizeof(tabulationTables[0][0]));
+	// Setup tabulation tables, used for the hashing.
+	unsigned ttsRows = (sizeof(tabulationTables)/sizeof(tabulationTables[0]));
+	unsigned ttsCols = (sizeof(tabulationTables[0])/sizeof(tabulationTables[0][0]));
 	for (unsigned i = 0; i < maxStrLen; i++)
 	{
-		for (unsigned j = 0; j < c; j++)
+		for (unsigned j = 0; j < ttsCols; j++)
 		{
 			// IF the max string length is shorter than the amount of random tables, use those.
-			if (maxStrLen <= r)
+			if (maxStrLen <= ttsRows)
 				tabulationTables[i][j].hashValue =1;
 			// Otherwise, calculate table entries using pseudorandom generator.
 			else
-				tabulationTables[i][j].hashValue = rand() % (1UL<<(sizeof(value_t) * 2) - 1);
+				tabulationTables[i][j].hashValue = generator() % (1UL<<(sizeof(value_t) * 8) - 1);
 		}
 	}
 }
 TabulationHash::~TabulationHash(void)
 {
-	unsigned r = (sizeof(tabulationTables)/sizeof(tabulationTables[0]));
-	for (unsigned i = 0; i < r; i++)
+	unsigned ttsRows = (sizeof(tabulationTables)/sizeof(tabulationTables[0]));
+	for (unsigned i = 0; i < ttsRows; i++)
 		delete[] tabulationTables[i];
 	delete[] tabulationTables;
 }
 
 value_t TabulationHash::getHash(string key)
 {
-	//const unsigned char * ukey = reinterpret_cast<const unsigned char *> (strVar.c_str());
+	// Possible solution for the silly hack below.
+	// const unsigned char * ukey = reinterpret_cast<const unsigned char *> (strVar.c_str());
 	value_t hashResult = 0;
 	for (int i = 0; i < key.size(); i += charLength)
 		hashResult ^= tabulationTables[i][((int)(key[i] + 128))].hashValue; //Silly hack to solve signed char in string problem
